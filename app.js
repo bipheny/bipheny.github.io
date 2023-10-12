21ed4706db05607f0d38f7b5f43bf32684a06206
Lokirovskii <719220502@qq.com>
2023-10-12 13:28:53 +0800

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+const contentContainer = document.getElementById('index-content');
+
+function injectContent(route) {
+  var htmlPath;
+  if (route === '/') {
+    htmlPath = './home.html';
+  } else {
+    htmlPath = '.' + route + '.html';
+  }
+  fetch(htmlPath)
+    .then((response) => response.text())
+    .then((data) => {
+      contentContainer.innerHTML = data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+injectContent(window.location.pathname);
+
+window.onpopstate = (event) => {
+  injectContent(event.state.path);
+};
+
+document.getElementById('index-nav-bar').childNodes.forEach((link) =>
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const route = event.target.getAttribute('href');
+    history.pushState({ path: route }, '', route);
+    injectContent(route);
+  })
+);
